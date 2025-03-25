2bcd2105a393215d225d72ba896c31ed1418dddb
abdus salam shehab <ashehab@hybri.tech>
2025-03-25 21:19:39 +0600

    added server listenign

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-
+require('dotenv').config();
 const app = express();
 app.use(cors());
 
 
 // listening port ===============
-
-
+const prot = 3000 || process.env.PORT;
+app.listen(prot , ()=>{
+    console.log('server listening successfully')
+})
 
